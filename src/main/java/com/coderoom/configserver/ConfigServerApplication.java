46d3c554add0207b2341aa4c0619b263f60eaaa6
Maxim Afanasiev <maxim.afanasiev@crossinx.com>
2024-03-02 14:33:17 +0200

    first commit

---
+++ src/main/java/com/coderoom/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.coderoom.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
